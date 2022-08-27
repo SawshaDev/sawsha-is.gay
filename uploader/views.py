b56dc8e77d341b72109312f61116d2689095a72a
SawshaDev <contactcallimarie@gmail.com>
2022-08-26 20:39:58 -0500

    Made strict changes overall + authentication

--- uploader/views.py
+++ uploader/views.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import string
 from typing import TYPE_CHECKING
@@ -11,6 +12,8 @@ from config import AUTH
 
 if TYPE_CHECKING:
     from starlette.datastructures import UploadFile, MultiDict
+
+logger = logging.getLogger(__name__)
 
 
 async def upload_file(request: Request) -> JSONResponse:
@@ -34,7 +37,7 @@ async def upload_file(request: Request) -> JSONResponse:
     if file is None:
         print("\033[91mReceived an upload request that has not given us a 'file' to upload\033[0m")
         content = {
-            "message": "No 'file' given.\nPlease make sure File Form Name is ``image`` not ``Image``"
+            "message": "No 'file' given.\nPlease make sure File Form Name is ``image`` not ``Image``x"
         }
         return JSONResponse(content, status_code=400)
 
@@ -58,15 +61,19 @@ async def upload_file(request: Request) -> JSONResponse:
 
 
 async def get_image(request: Request) -> Response:
-    file_id: str = request.path_params.get("name")
-    file_id = file_id.split(".")[0]
+    file_name: str = request.path_params.get("name")
+    try: 
+        file_ext = f"image/{file_name.split('.')[1]}"
+    except IndexError:
+        return JSONResponse({"error":"No extension found! please make sure there's an extension like ``.png``!"}, status_code=404)
+    file_id = file_name.split(".")[0]
 
 
     query = (
-        f"SELECT image, mime FROM images WHERE name = $1"
+        f"SELECT image, mime FROM images WHERE name = $1 AND mime = $2"
     )   
 
-    row = await request.app.pool.fetchrow(query, file_id)
+    row = await request.app.pool.fetchrow(query, file_id, file_ext)
     try:
         image = row["image"]
         mime = row["mime"]
