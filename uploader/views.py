c3c91e70c21907b96faec3d943ba44b5123140c2
cibere <71997063+cibere@users.noreply.github.com>
2022-10-08 13:18:17 -0700

    Fixed bug with getting video
    My fix only works with mp4 videos

--- uploader/views.py
+++ uploader/views.py
@@ -56,7 +56,8 @@ async def upload_file(request: Request) -> JSONResponse:
 async def get_image(request: Request) -> Response:
     file_name: str = request.path_params.get("name")
     try: 
-        file_ext = f"image/{file_name.split('.')[1]}"
+        ext = file_name.split('.')[1]
+        file_ext = f"image/{file_ext}" if file_ext != "mp4" else f"video/{file_ext}"
     except IndexError:
         return JSONResponse({"error": "No extension found! please make sure there's an extension like ``.png``!"}, status_code=404)
     file_id = file_name.split(".")[0]
